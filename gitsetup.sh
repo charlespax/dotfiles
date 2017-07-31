5f419dbdceb242854a5c269209fdfc2bb58e4191
Charles Edward Pax <charles.pax@gmail.com>
2017-07-31 09:22:14 -0400

    Configure git for simple pushing.

--- gitsetup.sh
+++ gitsetup.sh
@@ -4,6 +4,9 @@
 git config --global user.email "charles.pax@gmail.com"
 git config --global user.name "Charles Edward Pax"
 
+# Push only the current branch rather than all branches when using 'git push'
+git config --global push.default simple
+
 # Use vim as difftool
 git config --global diff.tool vimdiff
 git config --global difftool.prompt false
