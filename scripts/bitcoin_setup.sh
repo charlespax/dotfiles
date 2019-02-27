2ff2501f63849fabb24fe7f20ba0dc0b4d64a156
Charles Edward Pax <charles.pax@gmail.com>
2019-02-27 00:40:33 -0500

    Changed some language.

--- scripts/bitcoin_setup.sh
+++ scripts/bitcoin_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# This section checks to see if bitcoind is already installed
 printf "Checking for bitcoind installation... "
 INSTALLED="$(command -v bitcoind)"
 if [ "${INSTALLED##*/}" = "bitcoind" ]; then
@@ -12,15 +13,16 @@ else
     sudo apt-get -y install bitcoind bitcoin-qt
 fi
 
-# Setup lnd
+# Download and verify the go language installation file
 GO_FILENAME="go1.11.5.linux-amd64.tar.gz"
 GO_URL="https://dl.google.com/go/"
 GO_FILEURL=$GO_URL/$GO_FILENAME
 GO_HASH=`echo "ff54aafedff961eb94792487e827515da683d61a5f9482f668008832631e5d25"`
-
+echo Downloading GO... 
 wget -qc --show-progress $GO_FILEURL 
 HASH="`sha256sum go1.11.5.linux-amd64.tar.gz | awk -F \" \" '{ print $1 }'`"
-echo "$HASH"
+echo Downloading GO... COMPLETE
+printf "Verifying GO sha256... " 
 if [ "$HASH" = "$GO_HASH" ]; then
-    echo "Go language hash passed"
+    echo "PASSED"
 fi
