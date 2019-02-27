0ab55df8589c49608cb52364644ee78b2c83a876
Charles Edward Pax <charles.pax@gmail.com>
2019-02-27 13:20:39 -0500

    Change to bitcoin script.

--- scripts/bitcoin_setup.sh
+++ scripts/bitcoin_setup.sh
@@ -43,16 +43,25 @@ GO_FILENAME="go1.11.5.linux-amd64.tar.gz"
 GO_URL="https://dl.google.com/go/"
 GO_FILEURL=$GO_URL/$GO_FILENAME
 GO_HASH=`echo "ff54aafedff961eb94792487e827515da683d61a5f9482f668008832631e5d25"`
-echo Downloading GO... 
-wget -qc --show-progress $GO_FILEURL 
-HASH="`sha256sum go1.11.5.linux-amd64.tar.gz | awk -F \" \" '{ print $1 }'`"
-echo Downloading GO... COMPLETE
-printf "Verifying GO sha256... " 
-if [ "$HASH" = "$GO_HASH" ]; then
-    echo "PASSED"
+
+download_go() {
+    echo Downloading GO... 
+    wget -qc --show-progress $GO_FILEURL 
+    HASH="`sha256sum go1.11.5.linux-amd64.tar.gz | awk -F \" \" '{ print $1 }'`"
+    echo Downloading GO... COMPLETE
+    printf "Verifying GO sha256... " 
+    if [ "$HASH" = "$GO_HASH" ]; then
+        echo "PASSED"
+    else
+        echo "ERROR: incorrect sha256 hash"
+        exit 1
+    fi
+}
+
+if [ -f $GO_FILENAME ]; then
+    print "$GO_FILENAMER found"
 else
-    echo "ERROR: incorrect sha256 hash"
-    exit 1
+    download_go
 fi
 
 # Install GO
