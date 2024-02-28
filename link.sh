e57260aa909eb3b066fe58eb14889ef3d7741fbb
Charles Edward Pax <charles.pax@gmail.com>
2024-02-27 20:17:31 -0500

    Moved scripts to a scripts folder.

--- link.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-PWD=`pwd`
-ln -f -s $PWD/vimrc ~/.vimrc
-ln -f -s $PWD/scimrc ~/.scimrc
-ln -f -s $PWD/bash_profile ~/.bash_profile
-ln -f -s $PWD/bash_profile ~/.bashrc
-mkdir ~/.moc
-ln -f -s $PWD/moc ~/.moc/config
--- scripts/setup_git.sh
+++ scripts/setup_git.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+# This script configures git.
+
+# Get personal information from the user
+echo "Enter your name: "
+read NAME
+
+echo "Enter your email: "
+read EMAIL
+
 # Configure user name and email
-git config --global user.email "charles.pax@gmail.com"
-git config --global user.name "Charles Edward Pax"
+git config --global user.email "$NAME"
+git config --global user.name "$EMAIL"
 
 # Push only the current branch rather than all branches when using 'git push'
 git config --global push.default simple
@@ -12,3 +21,4 @@ git config --global diff.tool vimdiff
 git config --global difftool.prompt false
 git config --global alias.d difftool
 git config --global commit.gpgsign true
+
